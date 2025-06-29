26122053c9f1e90bcfea1d5bd1a5c6c82dab25e8
elahe870 <es.haghighat95@gmail.com>
2025-06-29 06:59:57 +0000

    second

--- app.py
+++ app.py
@@ -9,7 +9,20 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        return render_template("sort.html", name=request.form.get("name"))
+        coin_ids=request.form.get("name").strip().split(',')
+        try:
+            sorted_cryptos = sort_cryptos_by_potential(coin_ids)
+            
+            # نمایش نتایج
+            if sorted_cryptos:
+                print("\nSorted Cryptocurrencies by Potential:")
+                for crypto in sorted_cryptos:
+                    print(f"{crypto['name']} ({crypto['symbol']}): Score = {crypto['score']:.2f}")
+            else:
+                print("No valid data found for the provided cryptocurrencies.")
+        except Exception as e:
+            sys.exit(f"An unexpected error occurred: {e}")
+        return render_template("sort.html", sorted_cryptos=sorted_cryptos)
     return render_template("index.html")
 
 def oldmain():
