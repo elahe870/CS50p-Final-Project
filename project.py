f3bbf564eab58b1ce31807337c72d245c9d79ded
elahe870 <es.haghighat95@gmail.com>
2025-07-18 10:56:40 +0330

    11th commit

--- project.py
+++ project.py
@@ -1,5 +1,37 @@
 import requests
 import sys
+#from score1 import calculate_score
+#from score1 import sort_cryptos_by_potential
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        
+        coin_ids = extract_coin_ids(request.form.get("name"))
+        try:
+
+            sorted_cryptos = sort_cryptos_by_potential(coin_ids)
+            
+            # نمایش نتایج
+            if sorted_cryptos:
+                print("\nSorted Cryptocurrencies by Potential:")
+                for crypto in sorted_cryptos:
+                    print(f"{crypto['name']} ({crypto['symbol']}): Score = {crypto['score']:.2f}")
+            else:
+                print("No valid data found for the provided cryptocurrencies.")
+        except Exception as e:
+            sys.exit(f"An unexpected error occurred: {e}")
+        return render_template("sort.html", sorted_cryptos=sorted_cryptos)
+    else:
+        return render_template("index.html")
+
+def extract_coin_ids(form_data):
+    #extract input coins
+    return form_data.strip().split(',')
+
 
 def calculate_score(data):
     try:
@@ -56,5 +88,8 @@ def sort_cryptos_by_potential(coin_ids):
     return results
 
 
+def main():
+    app.run()
 
-
+if __name__ == "__main__":
+    main()
